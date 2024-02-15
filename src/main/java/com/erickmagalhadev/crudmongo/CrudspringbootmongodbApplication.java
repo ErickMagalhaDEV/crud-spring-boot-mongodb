077a1f872e57634004183315236eef647085c8da
ErickMagalhaDEV <erickmagalhadev@gmail.com>
2024-02-15 19:28:45 -0300

    Primeiro commit - projeto criado

---
+++ src/main/java/com/erickmagalhadev/crudmongo/CrudspringbootmongodbApplication.java
@@ -0,0 +1,13 @@
+package com.erickmagalhadev.crudmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudspringbootmongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudspringbootmongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/erickmagalhadev/crudmongo/CrudspringbootmongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erickmagalhadev.crudmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudspringbootmongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
